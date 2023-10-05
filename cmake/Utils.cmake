14e40d6b7405671193d9de692851e370987a1a06
silverhawke249 <hi@silverhawke.xyz>
2023-10-05 22:25:54 +0700

    Adjust assembly instructions on Windows

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -4,9 +4,10 @@ function(embed_resource TARGET INPUT_FILE)
     set(SYMBOL_NAME ${TARGET})
     string(REGEX REPLACE "[^a-zA-Z0-9]" "_" SYMBOL_NAME ${SYMBOL_NAME})
 
-    file(GENERATE
-        OUTPUT gen/${TARGET}-embedded.S
-        CONTENT "    .global ${SYMBOL_NAME}_Begin
+    if(UNIX)
+        file(GENERATE
+            OUTPUT gen/${TARGET}-embedded.S
+            CONTENT "    .global ${SYMBOL_NAME}_Begin
     .global ${SYMBOL_NAME}_End
     .section .note.GNU-stack,\"\",@progbits
     .rodata
@@ -14,6 +15,17 @@ ${SYMBOL_NAME}_Begin:
     .align 4
     .incbin \"${INPUT_FILE}\"
 ${SYMBOL_NAME}_End:")
+    else()
+        file(GENERATE
+            OUTPUT gen/${TARGET}-embedded.S
+            CONTENT "    .global ${SYMBOL_NAME}_Begin
+    .global ${SYMBOL_NAME}_End
+    .section .rodata
+${SYMBOL_NAME}_Begin:
+    .align 4
+    .incbin \"${INPUT_FILE}\"
+${SYMBOL_NAME}_End:")
+    endif()
 
     file(GENERATE
         OUTPUT gen/${TARGET}-embedded.h
